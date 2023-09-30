c9354ce0b5f2aad7f89265108c2e1f0c5523e714
Balqis Shafira Aini <balqisshafiraaini@gmail.com>
2023-09-30 21:54:45 +0700

    Initial Commit

---
+++ Melodissimo/MelodissimoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MelodissimoApp.swift
+//  Melodissimo
+//
+//  Created by Balqis on 30/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MelodissimoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
